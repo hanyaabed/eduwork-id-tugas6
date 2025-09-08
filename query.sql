fb69e39991695b6410d5c672497a3bbf35b4c7a3
hanyaabed <123574036+hanyaabed@users.noreply.github.com>
2025-09-08 14:59:48 +0700

    Tambah kode use database

--- query.sql
+++ query.sql
@@ -1,7 +1,10 @@
 -- 1. Membuat database ecommerce
 CREATE DATABASE ecommerce;
 
--- 2. Membuat tabel products
+-- 2. Masuk database ecommerce
+USE ecommerce;
+
+-- 3. Membuat tabel products
 CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nama_produk VARCHAR(255),
@@ -10,7 +13,7 @@ CREATE TABLE IF NOT EXISTS products (
     stok INT
 );
 
--- 3. Membuat tabel users
+-- 4. Membuat tabel users
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nama VARCHAR(255),
@@ -18,7 +21,7 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255)
 );
 
--- 4. Membuat table orders
+-- 5. Membuat table orders
 CREATE TABLE IF NOT EXISTS orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
@@ -29,24 +32,24 @@ CREATE TABLE IF NOT EXISTS orders (
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
--- 5. CREATE (menambahkan data produk)
+-- 6. CREATE (menambahkan data produk)
 INSERT INTO `products` (`nama_produk`, `harga`, `deskripsi`, `stok`) VALUES
 ('Baju', 5000, 'Kain tipis cocok di cuaca panas', 10), ('Celana', 6000, 'Celana panjang bahan dari kain', 10);
 
--- 6. READ (menampilkan data produk)
+-- 7. READ (menampilkan data produk)
 SELECT * FROM products;
 
--- 7. UPDATE (mengubah data produk)
+-- 8. UPDATE (mengubah data produk)
 UPDATE products 
 SET nama_produk = 'Baju Polos' 
 WHERE id = 1;
 
--- 8. READ lagi untuk memastikan perubahan setelah update
+-- 9. READ lagi untuk memastikan perubahan setelah update
 SELECT * FROM products;
 
--- 9. DELETE (menghapus data produk)
+-- 10. DELETE (menghapus data produk)
 DELETE FROM products 
 WHERE id = 2;
 
--- 10. READ lagi untuk memastikan perubahan setelah delete
+-- 11. READ lagi untuk memastikan perubahan setelah delete
 SELECT * FROM products;
